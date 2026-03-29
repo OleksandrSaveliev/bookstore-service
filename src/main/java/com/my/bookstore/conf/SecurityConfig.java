f131c60f2724d89b35e7d1b63a0041a7f889f38a
OleksandrSaveliev <a.savelev.krm@gmail.com>
2026-03-29 17:42:03 +0300

    fix base config

--- src/main/java/com/my/bookstore/conf/SecurityConfig.java
+++ src/main/java/com/my/bookstore/conf/SecurityConfig.java
@@ -5,9 +5,15 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+
+import static org.springframework.security.config.Customizer.withDefaults;
 
 @Configuration
 public class SecurityConfig {
@@ -23,9 +29,20 @@ public class SecurityConfig {
                         .requestMatchers("/","/api/login").permitAll()
                         .anyRequest().authenticated()
                 )
-
-                .formLogin(AbstractHttpConfigurer::disable);
+                .formLogin((withDefaults()));
+//                .formLogin(AbstractHttpConfigurer::disable);
         return http.build();
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        UserDetails userDetails = User
+                .withUsername("user")
+                .password(passwordEncoder().encode("123"))
+                .roles("USER")
+                .build();
+
+        return new InMemoryUserDetailsManager(userDetails);
     }
 
     @Bean
